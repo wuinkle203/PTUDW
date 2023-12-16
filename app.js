c88d229c22455937c02f7244ba0e49092914b11d
wuinkle203 <leq484437@gmai.com>
2023-12-16 20:40:58 +0700

    Cai dat express chay thong bo xin chao

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+    res.json({massage: "Welcome to contact book application."});
+});
+
+module.exports = app;
