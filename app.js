84bcc2929b0fe011b508976772ac111757c51acf
wuinkle203 <leq484437@gmai.com>
2023-12-16 20:48:51 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 
 
